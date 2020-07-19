7daab0ed849b2a67ab9d20edb05f1866fe9ef8c5
Andrew A. Stenipanin <and5251@yandex.ru>
2020-07-19 18:51:18 +0300

    editing App, QuestionCard Components, api request

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,10 +1,14 @@
-import * as React from "react";
+import React from 'react';
+// Types
+import { AnswerObject } from '../App';
+// Styles
+
 
 type Props = {
   question: string;
   answers: string[];
-  callback: any;
-  userAnswer: any;
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
   questionNr: number;
   totalQuestions: number;
 };
@@ -16,24 +20,62 @@ const QuestionCard: React.FC<Props> = ({
   userAnswer,
   questionNr,
   totalQuestions,
-}) => {
-  return (
+}) => (
+  <>
+    <p className='number'>
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
     <div>
-      <p className="number">
-        Question: {questionNr} / {totalQuestions}
-      </p>
-      <p dangerouslySetInnerHTML={{ __html: question }}></p>
-      <div>
-        {answers.map((answer) => (
-          <div>
-            <button disabled={userAnswer} onClick={callback}>
-              <span dangerouslySetInnerHTML={{ __html: answer }} />
-            </button>
-          </div>
-        ))}
-      </div>
+      {answers.map((answer) => (
+
+          <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+      ))}
     </div>
-  );
-};
+  </>
+);
 
 export default QuestionCard;
+
+
+// import * as React from "react";
+
+// type Props = {
+//   question: string;
+//   answers: string[];
+//   callback: any;
+//   userAnswer: any;
+//   questionNr: number;
+//   totalQuestions: number;
+// };
+
+// const QuestionCard: React.FC<Props> = ({
+//   question,
+//   answers,
+//   callback,
+//   userAnswer,
+//   questionNr,
+//   totalQuestions,
+// }) => {
+//   return (
+//     <div>
+//       <p className="number">
+//         Question: {questionNr} / {totalQuestions}
+//       </p>
+//       <p dangerouslySetInnerHTML={{ __html: question }}></p>
+//       <div>
+//         {answers.map((answer) => (
+//           <div key={answer}>
+//             <button disabled={userAnswer} value={answer} onClick={callback}>
+//               <span dangerouslySetInnerHTML={{ __html: answer }} />
+//             </button>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default QuestionCard;

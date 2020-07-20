7e37330c20711dfb7736a2ecde319edf61c9ebad
Andrew A. Stenipanin <and5251@yandex.ru>
2020-07-21 01:11:05 +0300

    Updates

---
+++ precache-manifest.79ac6d4e90f82cb76ce3dfcb781aa25a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084ea4f355ac25a070c5cda763f4e47b",
+    "url": "/quiz_app/index.html"
+  },
+  {
+    "revision": "8e5dff18701be116b927",
+    "url": "/quiz_app/static/js/2.e986474a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz_app/static/js/2.e986474a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "888d4a9518ef618dd798",
+    "url": "/quiz_app/static/js/main.34639045.chunk.js"
+  },
+  {
+    "revision": "4d8907cca5e0abe53a3b",
+    "url": "/quiz_app/static/js/runtime-main.461f4530.js"
+  },
+  {
+    "revision": "c11f97a3b321dad2ede69652d3f8a174",
+    "url": "/quiz_app/static/media/nattu-adnan-unsplash.c11f97a3.jpg"
+  }
+]);
